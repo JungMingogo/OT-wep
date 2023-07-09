a201b489445e4e6cbb7061a7d11b69f1cd0cda5e
JungMingogo <wjdals7282@naver.com>
2023-07-10 00:05:17 +0900

    로그인, 회원가입 화면 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -21,11 +21,12 @@ class UserStorage {
   static getUserInfo(id) {
     const users = this.#users;
     const idx = user.id.indexOf(id);
-    const userKeys = Object.keys(users);  // =>[id, psword, name]
-    const userInfo = userKeys.reduce((newUsers, info) => {
-        newUsers[info] = users[info][idx];
-        return newUsers;
+    const usersKeys = Object.keys(users);  // =>[id, psword, name]
+    const userInfo = usersKeys.reduce((newUser, info) => {
+        newUser[info] = users[info][idx];
+        return newUser;
     }, {});
+    
     return userInfo;
   }
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -3,43 +3,26 @@
 const User = require("../../models/User");
 
 const output = {
-    home: (req, res) => {
-        res.render("home/index");
-    },
-    
-    login: (req, res) => {
-        res.render("home/login");
-    },
-};
-
-const users = {
-    id: ["woorimIT", "나개발", "김팀장"],
-    psword: ["1234", "1234", "123456"],
+  home: (req, res) => {
+    res.render("home/index");
+  },
+  login: (req, res) => {
+    res.render("home/login");
+  },
+  register: (req, res) => {
+    res.render("home/register");
+  },
 };
 
 const process = {
-    login: (req, res) => {
-        const id = req.body.id;
-        const psword = req.body.psword;
-        
-        const users = UserSrorage.getUser("id", "psword");
-
-        const response = {};
-        if (users.id.includes(id)){
-            const idx = users.id.indexOf(id);
-            if(users.psword[idx] === psword) {
-                response.success = true;
-                return res.json(response);
-            }
-        }
-
-        response.success = false;
-        response.msg = "로그인에 실패하셨습니다.";
-        return res.json(response);
-    }
+  login: (req, res) => {
+    const user = new User(req.body);
+    const response = user.login();
+    return res.json(response);
+  },
 };
 
 module.exports = {
-    output, 
-    process,
+  output,
+  process,
 };
